44126625e208bde067c1b5aa9fbad4c606d925e3
rldyfh <rldyfh@gmail.com>
2022-11-19 20:20:11 +0900

    first commit

---
+++ src/views/WriteView.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from "@vue/reactivity";
+
+const axios = require('axios')
+
+const title = ref("")
+const content = ref("")
+const write = function() {
+    
+
+}
+
+</script>
+
+<template>
+    <div>
+        <el-input v-model="title" type="text" placeholder="제목을 입력해주세요." />
+    </div>
+    
+    <div class="align-items-center">
+        <div class="mt-2">
+            <el-input v-model="content" type="textarea" rows="15" />
+        </div>
+
+        <div class="mt-2">
+            <el-button type="primary" @click="write()">글 작성완료</el-button>
+        </div>
+    </div>
+
+</template>
+
+<style>
+
+</style>
